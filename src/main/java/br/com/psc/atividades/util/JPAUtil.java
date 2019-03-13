5ef3ceac6d9f61ecd78093b4291401ff736db1dc
rodolfogomes <rodolfoaguiargomes@gmail.com>
2019-03-13 17:22:16 -0300

    Classe utiliraria para gerar o entitymanager

---
+++ src/main/java/br/com/psc/atividades/util/JPAUtil.java
@@ -0,0 +1,26 @@
+package br.com.psc.atividades.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	private EntityManagerFactory factory;
+	private static JPAUtil instance;
+	
+	private JPAUtil() {
+		this.factory = Persistence.createEntityManagerFactory("unit-mysql");
+	}
+	
+	public JPAUtil getInstance() {
+		if(instance == null) {
+			instance = new JPAUtil();
+		}
+		return instance;
+	}
+	
+	public EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
